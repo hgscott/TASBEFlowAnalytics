34aa1c064ba51753610da6f0b81974f3556b9534
jakebeal <jakebeal@alum.mit.edu>
2018-02-09 10:29:37 -0600

    fix issue #149

--- code/get_bead_peaks.m
+++ code/get_bead_peaks.m
@@ -52,7 +52,7 @@ function returned = getBeadCatalog(forceReload)
     if nargin<1, forceReload=false; end;
     if isempty(catalog) || forceReload,
         % ISSUE-81: The cell range of the spreadsheet must be updated whenever BeadCatalog.xlsx is updated.
-        [nums txts combo] = xlsread([fileparts(mfilename('fullpath')) '/BeadCatalog.xlsx'], 1, 'A1:M95');
+        [nums txts combo] = xlsread([fileparts(mfilename('fullpath')) '/../BeadCatalog.xlsx'], 1, 'A1:M95');
         catalog = parseCatalog(combo);
     end
     returned = catalog;
