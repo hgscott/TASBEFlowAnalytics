619ea9a6fff6d0a009d982a9e64d6858cca7b042
jakebeal <jakebeal@alum.mit.edu>
2018-04-06 06:37:40 -0500

    add function for pulling channel names from an FCS file

--- tests/test_utilities.m
+++ tests/test_utilities.m
@@ -18,3 +18,15 @@ assertEqual(isolated_points([NaN 2 NaN 4 NaN]), logical([0 1 0 1 0]));
 assertEqual(isolated_points([1 NaN 3 -4 5],1), logical([1 0 1 0 1]));
 assertEqual(isolated_points([1 Inf 3 -Inf 5]), logical([1 0 1 0 1]));
 assertEqual(isolated_points([1 NaN 3 4 5]'), logical([1 0 0 0 0]));
+
+
+function test_fcs_channel_names
+
+stem1011 = '../TASBEFlowAnalytics-Tutorial/example_assay/LacI-CAGop_';
+names = fcs_channel_names([stem1011 'B5_B05_P3.fcs']);
+
+expectedNames = {'FSC-A', 'FSC-H', 'FSC-W', 'SSC-A', 'SSC-H', 'SSC-W',...
+    'FITC-A', 'PerCP-Cy5-5-A', 'PE-YG-A', 'PE-Tx-Red-YG-A', ...
+    'Pacific Blue-A', 'AmCyan-A', 'Time'};
+
+assertEqual(names,expectedNames);
