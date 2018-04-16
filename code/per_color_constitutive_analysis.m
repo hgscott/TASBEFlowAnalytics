df2a6e5c3354358c0eafa224a804d35f8ced51cf
jakebeal <jakebeal@alum.mit.edu>
2018-04-15 21:29:50 -0500

    add GMMs to batch analysis, fixing #69. Also serialize them into output CSVs

--- code/per_color_constitutive_analysis.m
+++ code/per_color_constitutive_analysis.m
@@ -6,7 +6,7 @@
 % exception, as described in the file LICENSE in the TASBE analytics
 % package distribution's top directory.
 
-function [results sampleresults] = per_color_constitutive_analysis(colorModel,batch_description,colors,AP)
+function [results, sampleresults] = per_color_constitutive_analysis(colorModel,batch_description,colors,AP)
 % The 'results' here is not a standard ExperimentResults, but a similar scratch structure
 
 warning('TASBE:UpdateNeeded','Need to update per_color_constitutive_analysis to use new samplestatistics');
@@ -37,6 +37,7 @@ results = cell(n_conditions,1); sampleresults = results;
 for i=1:n_conditions,
     replicatecounts = numel(rawresults{1}{i,2});
     samplebincounts = cell(replicatecounts,1);
+    sample_gmm_means = samplebincounts; sample_gmm_stds = samplebincounts; sample_gmm_weights = samplebincounts;
     samplemeans = zeros(replicatecounts,numel(colors)); samplestds = samplemeans;
     results{i}.condition = batch_description{i,1};
     results{i}.bincenters = bincenters;
@@ -47,18 +48,23 @@ for i=1:n_conditions,
         results{i}.bincounts(:,j) = rawbincounts;
         results{i}.means(j) = geomean(bincenters',rawbincounts);
         results{i}.stds(j) =  geostd(bincenters',rawbincounts);
+        [results{i}.gmm_means(:,j), results{i}.gmm_stds(:,j), results{i}.gmm_weights(:,j)] = get_channel_gmm_results(rawresults{j}{i,1},'constitutive');
         % per-sample histograms
         for k=1:replicatecounts,
             samplebincounts{k}(:,j) = SR{k}.BinCounts;
             samplemeans(k,j) = geomean(bincenters',SR{k}.BinCounts);
             samplestds(k,j) = geostd(bincenters',SR{k}.BinCounts);
+            color_column = find(colorModel,channel_named(colorModel,colors{j}));
+            sample_gmm_means{k}(:,j) = SR{k}.PopComponentMeans(:,color_column);
+            sample_gmm_stds{k}(:,j) = SR{k}.PopComponentStandardDevs(:,color_column);
+            sample_gmm_weights{k}(:,j) = SR{k}.PopComponentWeights(:,color_column);
         end
         results{i}.stdofmeans(j) = geostd(samplemeans(:,j));
         results{i}.stdofstds(j) = mean(samplestds(:,j));
     end
     for k=1:replicatecounts,
-        sampleresults{i}{k} = SampleResults([], [], [], samplebincounts{k}, samplemeans(k,:), samplestds(k,:), ...
-            [], [], [], [], [], [], [], [], [], [], [], []);
+        sampleresults{i}{k} = SampleResults([], batch_description{i,2}{k}, setChannelLabels(AP,colors'), samplebincounts{k}, samplemeans(k,:), samplestds(k,:), ...
+            [], [], [], [], [], [], [], [], [], sample_gmm_means{k}, sample_gmm_stds{k}, sample_gmm_weights{k});
     end
 end
 
