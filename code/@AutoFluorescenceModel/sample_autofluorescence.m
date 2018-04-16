b8f1374a65b7000979396b69d818f455aca0913a
jakebeal <jakebeal@alum.mit.edu>
2018-04-16 06:42:42 -0500

    fix AFM by refactoring autofluorescence sampling to be in both CM and AFM

--- code/@AutoFluorescenceModel/sample_autofluorescence.m
+++ code/@AutoFluorescenceModel/sample_autofluorescence.m
@@ -6,15 +6,12 @@
 % exception, as described in the file LICENSE in the TASBE analytics
 % package distribution's top directory.
 
-function AF = sample_autofluorescence(CM,n_samples,truncate,channels)
-
-n_channels = numel(getChannels(CM));
+function AF = sample_autofluorescence(AFM,n_samples,truncate)
 
 if nargin<3, truncate = 0; end;
-if nargin<4, channels = 1:n_channels; end;
 
-AF = zeros(n_samples,numel(channels));
-for i=1:numel(channels),
-    AFM = get_autofluorescence_model(CM,channels(i));
-    AF(:,i) = sample_autofluorescence(AFM,n_samples,truncate);
+AF = getMeanERF(AFM) + getStdERF(AFM)*randn(n_samples,1);
+
+if truncate
+    AF(AF<=1) = 1;
 end
