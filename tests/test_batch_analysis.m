11f17a144fd90c7a3b417a349b7355299c0254be
Cassandra Overney <coverney@olin.edu>
2018-07-03 11:57:09 -0400

    Requested changes to #275
    Added tests for linespecs not matching and to make sure 'k' is set at the correct time.

--- tests/test_batch_analysis.m
+++ tests/test_batch_analysis.m
@@ -206,3 +206,57 @@ assertElementsAlmostEqual(results{1}.gmm_weights,  result_expected1_gmm_weights,
 assertElementsAlmostEqual(results{14}.gmm_means,  result_expected14_gmm_means,  'relative', 1e-2);
 assertElementsAlmostEqual(results{14}.gmm_stds,  result_expected14_gmm_stds,  'relative', 1e-2);
 assertElementsAlmostEqual(results{14}.gmm_weights,  result_expected14_gmm_weights,  'relative', 1e-2);
+
+function test_batch_analysis_plot_warnings
+% Test for warnings in plot_batch_histograms
+CM2 = load_or_make_testing_colormodel2();
+stem1011 = '../TASBEFlowAnalytics-Tutorial/example_assay/LacI-CAGop_';
+
+% set up metadata
+experimentName = 'LacI Transfer Curve';
+
+% Configure the analysis
+% Analyze on a histogram of 10^[first] to 10^[third] ERF, with bins every 10^[second]
+bins = BinSequence(4,0.1,10,'log_bins');
+
+% Designate which channels have which roles
+AP = AnalysisParameters(bins,{});
+% Ignore any bins with less than valid count as noise
+AP=setMinValidCount(AP,100');
+% Ignore any raw fluorescence values less than this threshold as too contaminated by instrument noise
+AP=setPemDropThreshold(AP,5');
+% Add autofluorescence back in after removing for compensation?
+AP=setUseAutoFluorescence(AP,false');
+
+% Make a map of condition names to file sets
+file_pairs = {...
+  'Dox 0.1',    {[stem1011 'B3_B03_P3.fcs']}; % Replicates go here, e.g., {[rep1], [rep2], [rep3]}
+  'Dox 0.2',    {[stem1011 'B4_B04_P3.fcs']};
+  'Dox 0.5',    {[stem1011 'B5_B05_P3.fcs']};
+  'Dox 1.0',    {[stem1011 'B6_B06_P3.fcs']};
+  'Dox 2.0',    {[stem1011 'B7_B07_P3.fcs']};
+  'Dox 5.0',    {[stem1011 'B8_B08_P3.fcs']};
+  'Dox 10.0',   {[stem1011 'B9_B09_P3.fcs']};
+  'Dox 20.0',   {[stem1011 'B10_B10_P3.fcs']};
+  'Dox 50.0',   {[stem1011 'B11_B11_P3.fcs']};
+  'Dox 100.0',  {[stem1011 'B12_B12_P3.fcs']};
+  'Dox 200.0',  {[stem1011 'C1_C01_P3.fcs']};
+  'Dox 500.0',  {[stem1011 'C2_C02_P3.fcs']};
+  'Dox 1000.0', {[stem1011 'C3_C03_P3.fcs']};
+  'Dox 2000.0', {[stem1011 'C4_C04_P3.fcs']};
+  };
+
+n_conditions = size(file_pairs,1);
+
+% Execute the actual analysis
+[results, sampleresults] = per_color_constitutive_analysis(CM2,file_pairs,{'EBFP2','EYFP','mKate'},AP);
+
+% Make output plots
+TASBEConfig.set('OutputSettings.StemName','LacI-CAGop');
+TASBEConfig.set('plots.plotPath','/tmp/plots');
+TASBEConfig.set('OutputSettings.FixedInputAxis',[1e4 1e10]);
+plot_batch_histograms(results,sampleresults,CM2);
+log = TASBESession.list();
+assertEqual(log{end-1}.contents{1}.name, 'NoLineSpecs');
+assertExceptionThrown(@()plot_batch_histograms(results,sampleresults,CM2,{'b'}), 'plot_batch_histograms:LineSpecDimensionMismatch', 'No error was raised.');
+
