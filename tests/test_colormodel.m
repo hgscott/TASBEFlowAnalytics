3cb34e2b3ae771c81c74823d138e3c7b5b0a6d66
jakebeal <jakebeal@alum.mit.edu>
2019-04-03 16:04:42 -0500

    Ensure that flow.channel_template_file gets wrapped into a DataFile object when used by resolve

--- tests/test_colormodel.m
+++ tests/test_colormodel.m
@@ -10,8 +10,8 @@ function test_colormodel_endtoend
 
 stem0312 = '../TASBEFlowAnalytics-Tutorial/example_controls/2012-03-12_';
 
-beadfile = DataFile('fcs', [stem0312 'Beads_P3.fcs']);
-blankfile = DataFile('fcs', [stem0312 'blank_P3.fcs']);
+beadfile = [stem0312 'Beads_P3.fcs'];
+blankfile = [stem0312 'blank_P3.fcs'];
 
 % Autodetect gating with an N-dimensional gaussian-mixture-model
 AGP = AutogateParameters();
@@ -24,17 +24,17 @@ channels = {}; colorfiles = {};
 channels{1} = Channel('FITC-A', 488, 515, 20);
 channels{1} = setPrintName(channels{1}, 'EYFP'); % Name to print on charts
 channels{1} = setLineSpec(channels{1}, 'y'); % Color for lines, when needed
-colorfiles{1} = DataFile('fcs', [stem0312 'EYFP_P3.fcs']);
+colorfiles{1} = [stem0312 'EYFP_P3.fcs'];
 
 channels{2} = Channel('PE-Tx-Red-YG-A', 561, 610, 20);
 channels{2} = setPrintName(channels{2}, 'mKate');
 channels{2} = setLineSpec(channels{2}, 'r');
-colorfiles{2} = DataFile('fcs', [stem0312 'mkate_P3.fcs']);
+colorfiles{2} = [stem0312 'mkate_P3.fcs'];
 
 channels{3} = Channel('Pacific Blue-A', 405, 450, 50);
 channels{3} = setPrintName(channels{3}, 'EBFP2');
 channels{3} = setLineSpec(channels{3}, 'b');
-colorfiles{3} = DataFile('fcs', [stem0312 'ebfp2_P3.fcs']);
+colorfiles{3} = [stem0312 'ebfp2_P3.fcs'];
 
 % Multi-color controls are used for converting other colors into ERF units
 % Any channel without a control mapping it to ERF will be left in arbirary units.
@@ -42,8 +42,8 @@ colorpairfiles = {};
 % Entries are: channel1, channel2, constitutive channel, filename
 % This allows channel1 and channel2 to be converted into one another.
 % If you only have two colors, you can set consitutive-channel to equal channel1 or channel2
-colorpairfiles{1} = {channels{1}, channels{2}, channels{3}, DataFile('fcs', [stem0312 'mkate_EBFP2_EYFP_P3.fcs'])};
-colorpairfiles{2} = {channels{1}, channels{3}, channels{2}, DataFile('fcs', [stem0312 'mkate_EBFP2_EYFP_P3.fcs'])};
+colorpairfiles{1} = {channels{1}, channels{2}, channels{3}, [stem0312 'mkate_EBFP2_EYFP_P3.fcs']};
+colorpairfiles{2} = {channels{1}, channels{3}, channels{2}, [stem0312 'mkate_EBFP2_EYFP_P3.fcs']};
 
 CM = ColorModel(beadfile, blankfile, channels, colorfiles, colorpairfiles);
 CM=set_translation_plot(CM, true);
@@ -457,3 +457,39 @@ colorpairfiles = {};
 
 assertExceptionThrown(@()(ColorModel(beadfile, blankfile, channels, colorfiles, colorpairfiles)),...
     'TASBE:ColorModel:OneColorfilePerChannel', 'No error was raised');
+
+
+% Make sure manual setting of template file converts to datafile correctly
+function test_colormodel_channeltemplate
+
+TASBEConfig.checkpoint('test');
+
+stem0312 = '../TASBEFlowAnalytics-Tutorial/example_controls/2012-03-12_';
+
+beadfile = [stem0312 'Beads_P3.fcs'];
+blankfile = [stem0312 'blank_P3.fcs'];
+TASBEConfig.set('flow.channel_template_file',beadfile);
+
+% Create one channel / colorfile pair for each color
+channels = {}; colorfiles = {};
+channels{1} = Channel('PE-Tx-Red-YG-A', 561, 610, 20);
+channels{1} = setPrintName(channels{1}, 'mKate');
+channels{1} = setLineSpec(channels{1}, 'r');
+colorfiles{1} = [stem0312 'mkate_P3.fcs'];
+
+% Multi-color controls are used for converting other colors into ERF units
+% Any channel without a control mapping it to ERF will be left in arbirary units.
+colorpairfiles = {};
+
+CM = ColorModel(beadfile, blankfile, channels, colorfiles, colorpairfiles);
+
+TASBEConfig.set('beads.beadModel','SpheroTech RCP-30-5A'); % Entry from BeadCatalog.xls matching your beads
+TASBEConfig.set('beads.beadBatch','Lot AA01, AA02, AA03, AA04, AB01, AB02, AC01, GAA01-R'); % Entry from BeadCatalog.xls containing your lot
+TASBEConfig.set('beads.beadChannel','PE-TR');
+CM=set_ERF_channel_name(CM, 'PE-Tx-Red-YG-A');
+
+TASBEConfig.set('plots.plotPath', '/tmp/plots');
+% Execute and save the model
+CM=resolve(CM);
+save('-V7','/tmp/CM120312.mat','CM');
+
