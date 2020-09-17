d66b6e4eb351a79d8a247a9ed9c8ab277a44f620
Jake Beal <jakebeal@alum.mit.edu>
2020-09-17 15:50:55 -0500

    add a second test

--- tests/test_small_fcs_files.m
+++ tests/test_small_fcs_files.m
@@ -32,7 +32,6 @@ TASBEConfig.set('calibration.overrideUnits',1);
 % Execute and save the model
 CM=resolve(CM);
 
-
 % Configure the analysis
 bins = BinSequence(0,0.1,10,'log_bins');
 AP = AnalysisParameters(bins,{});
@@ -40,6 +39,56 @@ AP = AnalysisParameters(bins,{});
 % Make a map of condition names to file sets
 file_pairs = {...
     'small1', {fcs0events, fcs1events, fcs79events, fcs2events};
+    'small2', {fcs79events, fcs2events};
+  };
+
+[results, sampleresults] = per_color_constitutive_analysis(CM,file_pairs,{'GFP'},AP);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Check results:
+
+assertEqual(numel(results), 2);
+assertElementsAlmostEqual(results{1}.n_events, [1 1 79 2],1e-2);
+assertElementsAlmostEqual(results{2}.n_events, [79 2],1e-2);
+
+
+
+function test_small_fcs_filtered
+
+% two micro-files, one with 0 events, one with 2 events
+fcs0events = '../TASBEFlowAnalytics-Tutorial/tests/additional_test_files/aq1endun9epg7tm.fcs';
+fcs1events = '../TASBEFlowAnalytics-Tutorial/tests/additional_test_files/aq1endun9efp22g.fcs';
+fcs2events = '../TASBEFlowAnalytics-Tutorial/tests/additional_test_files/aq1enduncr9sukn.fcs';
+fcs79events = '../TASBEFlowAnalytics-Tutorial/tests/additional_test_files/aq1endunfwb9kmg.fcs';
+
+blankfile = fcs0events;
+
+% Create one channel / colorfile pair for each color
+channels = {}; colorfiles = {};
+channels{1} = Channel('BL1-A', 488, 515, 20);
+channels{1} = setPrintName(channels{1}, 'GFP'); % Name to print on charts
+channels{1} = setLineSpec(channels{1}, 'g'); % Color for lines, when needed
+colorfiles{1} = fcs79events;
+
+CM = ColorModel([], blankfile, channels, colorfiles, {});
+
+CM=set_ERF_channel_name(CM, 'BL1-A');
+TASBEConfig.set('flow.channel_template_file',blankfile);
+TASBEConfig.set('calibration.overrideUnits',1);
+
+% Execute and save the model
+RF = RangeFilter('BL1-A',[20 21]);
+CM=resolve(CM);
+
+CM = add_prefilter(CM,RF);
+
+% Configure the analysis
+bins = BinSequence(0,0.1,10,'log_bins');
+AP = AnalysisParameters(bins,{});
+
+% Make a map of condition names to file sets
+file_pairs = {...
+    'small1', {fcs0events, fcs1events, fcs2events};
     'small2', {fcs79events, fcs2events};
   };
 
